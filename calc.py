c4bf1e8026e442be3540484ac8cefe2e0ce1deac
Hussain Jawaid <hussainjawaid.work@gmail.com>
2026-01-12 00:07:59 +0500

    reduce redanduncy in code

--- calc.py
+++ calc.py
@@ -1,7 +1,3 @@
-num1 = input("Enter your first number: ")
-num2 = input("Enter your second number: ")
-operation = input("What operation you wanna be performed? ")
-
 def add(x ,y):
     return x + y
 
