4fc749a8ca49ec7d6f8b1f5078bd1e7fe70007ee
Hussain Jawaid <hussainjawaid.work@gmail.com>
2026-01-11 11:56:14 +0500

    Add user input handling for calculator

--- calc.py
+++ calc.py
@@ -1,3 +1,7 @@
+num1 = input("Enter your first number: ")
+num2 = input("Enter your second number: ")
+operation = input("What operation you wanna be performed? ")
+
 def add(x ,y):
     return x + y
 
